46855536119cb8eca1b697e56e56bac202424490
wonseok <gratus907@snu.ac.kr>
2021-01-06 21:28:35 +0900

    blog
    Former-commit-id: cd2ea9feabf750f2ce4abab62ed30b9365a8f62c

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": ["jekyll","blog-init"],
+        "url": "http://localhost:4000/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Test some post!",
+        "excerpt":"lorem ipsum dolor sit amet, consectetur adipiscing elitlorem ipsum dolor sit amet, consectetur adipiscing elitlorem ipsum dolor sit amet, consectetur adipiscing elitlorem ipsum dolor sit amet, consectetur adipiscing elitlorem ipsum dolor sit amet, consectetur adipiscing elitlorem ipsum dolor sit amet, consectetur adipiscing elitlorem ipsum dolor sit amet, consectetur adipiscing elitlorem...","categories": ["jekyll","update"],
+        "tags": ["jekyll","blog-init"],
+        "url": "http://localhost:4000/jekyll/update/some-post/",
+        "teaser": null
+      }]

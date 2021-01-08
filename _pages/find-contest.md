7e5aa0c1ce6cd60fa3c7367f0c0339a46847252b
gratus907 <gratus907@naver.com>
2021-01-09 07:03:26 +0900

    'boj18194'
    Former-commit-id: 5efe89fda2c7c0cdea86dd3b8eeafddaac3e1dd2

---
+++ _pages/find-contest.md
@@ -0,0 +1,18 @@
+---
+title: "알고리즘 문제풀이 포스팅 대회별로 보기"
+layout: single
+permalink: /find-contest/
+sidebar:
+    nav: "sidepost"
+author_profile: true
+---
+Jekyll 블로그를 만들기로 한 가장 큰 이유는 이렇게 생긴 페이지가 만들고 싶어서였습니다 :)  
+막상 보니 깎아야 할 CSS가 너무 많네요..
+
+## 경기과학고
+
+#### 2019
+
+| 번호 |문제 | 포스팅 링크 |
+| ----------- | ----------- | ----------- |
+| **J** | Bad Hair Day와 기댓값 | [링크](/algorithms/BOJ18194/)|
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
         "title": "Hello, Jekyll!",
-        "excerpt":"Jekyll Blog  ","categories": ["jekyll"],
+        "excerpt":"Jekyll Blog Jekyll은 Markdown을 이용해서 (주로 이런 주제의) 블로그를 쉽게 작성할 수 있도록 해주는 놀라운 툴입니다. Theme을 원하는 대로 적용해서 커스터마이즈 할 수 있고, 저는 아직 잘 모르겠지만 .scss 파일을 잘 만지면 생각보다 많이 손댈 수 있어 보입니다. Why Jekyll? 저는 기존에 네이버 블로그와 지금 쓰고 있는 티스토리 링크 에서...","categories": ["jekyll"],
         "tags": ["jekyll"],
         "url": "http://localhost:4000/jekyll/Hello-Jekyll/",
+        "teaser": null
+      },{
+        "title": "BOJ 18194, 경기과학고 2019 Bad Hair Day와 기댓값",
+        "excerpt":"난이도 : Platinum 1 출처 : 경기과학고 프로그래밍 대회 2019 풀이 각 소 $i$ 에 대해, $n!$ 개의 가능한 배치 중 $i$ 가 $j$ 를 볼 수 있는 경우의 수를 센다고 생각하자. 이때 이 값을 $f(i, j)$ 라고 하면, 우리가 구하는 값은 (기댓값의 선형성에 의해) \\(\\frac{1}{n!}\\sum_{i, j} f(i, j)\\) 가...","categories": ["algorithms"],
+        "tags": ["boj","경기과학고-2019"],
+        "url": "http://localhost:4000/algorithms/BOJ18194/",
         "teaser": null
       }]
